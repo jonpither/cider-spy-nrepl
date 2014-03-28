c2327cf977d36ed865c02ea13ea17e7f42c5c345
Jon Pither <jon.pither@gmail.com>
2014-03-28 08:55:47 +0000

    Test that breaks because nrepl-server to hub connections should be session bound.

--- test/cider_spy_nrepl/sessions_test.clj
+++ test/cider_spy_nrepl/sessions_test.clj
@@ -12,6 +12,7 @@
 ;; TODO Make very clear tests that cover 2 sessions to same nrepl server
 ;; TODO test 2 connectons through same nrepl to diff hubs
 ;;  I should manually test what happenz if 2 diff emacs CIDERs with diff aliases connect to same nrepl-server then on to the hub.
+;; TODO fn on client-facade - other users (uses diff between session-ids)
 
 (defmacro test-with-server [& forms]
   `(let [~'server (hub-server/start-netty-server :port 9812)]
@@ -21,27 +22,41 @@
        (finally
          (hub-server/shutdown ~'server)))))
 
-(defmacro test-with-client [alias & forms]
+(defmacro test-with-client [client-name alias & forms]
   `(do
      (reset! client-events/registrations #{})
      (let [~'session-id (str (UUID/randomUUID))
-           ~'client (client-facade/connect-to-hub! "localhost" 9812 ~alias ~'session-id)]
+           ~client-name (client-facade/connect-to-hub! "localhost" 9812 ~alias ~'session-id)]
        ;; Allow time for registration message to do a round trip
        (Thread/sleep 500)
        (try
          ~@forms
          (finally
-           (hubc/shutdown! ~'client))))))
+           (hubc/shutdown! ~client-name))))))
 
 (deftest test-client-should-register-and-unregister
   (test-with-server
    (test-with-client
-    "jonnyboy"
+    client1 "jonnyboy"
     (is (= #{"jonnyboy"} (set (register/aliases))))
     (is (= #{"jonnyboy"} @client-events/registrations))
 
-    (hubc/shutdown! client)
+    (hubc/shutdown! client1)
 
     (Thread/sleep 500)
 
     (is (= #{} (set (register/aliases)))))))
+
+(deftest test-two-registrations
+  (test-with-server
+   (test-with-client
+    client1 "jonnyboy"
+
+    (test-with-client
+     client2 "frank"
+
+     (is (= #{"jonnyboy" "frank"} @client-events/registrations)))
+
+    (Thread/sleep 500)
+
+    (is (= #{"jonnyboy"} (set (register/aliases)))))))

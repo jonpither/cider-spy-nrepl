7fb187d2452701f3add801c485d5e96c270ae99c
Jon Pither <jon.pither@gmail.com>
2014-04-01 09:01:55 +0100

    failing test

--- test/cider_spy_nrepl/interactions_test.clj
+++ test/cider_spy_nrepl/interactions_test.clj
@@ -4,30 +4,40 @@
             [cider-spy-nrepl.hub.client-events :as client-events]
             [cider-spy-nrepl.middleware.cider :as cider]))
 
-;; to test interactions between server and hub.
-;;  I need to be able to trigger events (from server to client).
-;;  I want to test the client summary.
+;; TODO - make this nicer and test for unregister event.
+;; TODO - Test the summary msg for registrations.
+;; TODO - this bypasses all the handler code which is fair enough. Perhaps need a dedicated test?
 
-;; fix up a channel for the server-events that wires straight in a client-events.
+(defn- send-to-client-events
+  "Stub out SEND-BACK-TO-CIDER! and capture what is sent."
+  [client-session op & {:as msg}]
+  (let [sent-to-client (promise)]
+    (binding [cider/send-back-to-cider!
+              (fn [transport session-id message-id s]
+                (deliver sent-to-client [transport session-id message-id s]))]
+      (client-events/process client-session (assoc msg :op op)))
+    @sent-to-client))
 
-;; TODO test each arg to send-back-to-cider!
-(defn- send-to-client-events [client-session op & {:as msg}]
-  (binding [cider/send-back-to-cider!
-            (fn [transport session-id message-id s]
-              (is (= (:transport @client-session) transport))
-              (is (= (:id @client-session) session-id))
-              (is (= (:summary-message-id @client-session) message-id)))]
-    (client-events/process client-session (assoc msg :op op))))
+(defn- process-on-server-and-client-receives
+  "Process SERVER-MSG as an incoming msg to the server.
+   We expect a msg to be sent back to the client, and for client to send
+   a message to CIDER."
+  [client-session server-msg]
+  (binding [server-events/broadcast-msg!
+            (partial send-to-client-events client-session)]
+    (let [server-session (atom {:id "fooid"})]
+      (server-events/process nil server-session server-msg))))
 
 (deftest registration-should-bubble-to-cider []
   (let [client-session
         (atom {:id "fooid"
                :transport :t1
                :summary-message-id 123})]
-    (binding [server-events/broadcast-msg!
-              (partial send-to-client-events client-session)]
-      (let [server-session (atom {:id "fooid"})]
-        (server-events/process nil server-session {:op :register})))))
+    (let [[transport session-id message-id s]
+          (process-on-server-and-client-receives
+           client-session {:op :register :alias "Jon"})]
+         (is (= (:transport @client-session) transport))
+         (is (= (:id @client-session) session-id))
+         (is (= (:summary-message-id @client-session) message-id))
 
-;: TODO - test that summary is automatically updated when someone registers / deregisters.
-;; TODO - need to test the correct code is called to asynchronously push a message to cider
+         (is (re-find #"Devs hacking\n   Jon" s)))))

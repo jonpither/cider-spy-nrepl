76487796b38a4d4277b217340a41dc55e6114c36
Jon Pither <jon.pither@gmail.com>
2014-04-07 13:50:25 +0100

    cider-spy-reset command for use with cider-spy

--- src/cider_spy_nrepl/middleware/cider_spy.clj
+++ src/cider_spy_nrepl/middleware/cider_spy.clj
@@ -11,6 +11,13 @@
     (cider/update-session-for-summary-msg! session msg)
     (cider/update-spy-buffer-summary! session)))
 
+(defn- handle-reset
+  "Reset CIDER-SPY tracking."
+  [msg]
+  (let [session (sessions/session! msg)]
+    (swap! session dissoc :tracking)
+    (cider/update-spy-buffer-summary! session)))
+
 (defn- wrap-tracking
   "Wrap the handler to apply tracking and to update the CIDER SPY summary buffer."
   [handler {:keys [transport session] :as msg}]
@@ -26,6 +33,7 @@
   (fn [{:keys [op] :as msg}]
     (condp = op
       "cider-spy-summary" (handle-summary msg)
+      "cider-spy-reset" (handle-reset msg)
       (wrap-tracking handler msg))))
 
 ;; TODO figure out what this actually done.
@@ -33,4 +41,6 @@
  #'wrap-cider-spy
  {:handles
   {"cider-spy-summary"
-   {:doc "Return a summary of hacking information about the nrepl session."}}})
+   {:doc "Return a summary of hacking information about the nrepl session."}
+   "cider-spy-reset"
+   {:doc "Reset all tracking information."}}})

f85369c870c7bc6db3065b95cf4b7c2ab1fa828d
frankiesardo <fran.sardo@gmail.com>
2014-05-27 18:26:04 +0200

    Fix not running test for top-nses

--- test/cider_spy_nrepl/ns_trail_test.clj
+++ test/cider_spy_nrepl/ns_trail_test.clj
@@ -3,11 +3,16 @@
             [clojure.test :refer :all])
   (import [org.joda.time LocalDateTime Seconds]))
 
-(deftest footest
-  (= [["boo" 2.7] ["boo2" 1.0] ["boo3" 0.7]]
-     (top-nses (LocalDateTime. 2010 1 1 0 30 0)
-               [{:dt (LocalDateTime. 2010 1 1 0 14 0) :ns "boo3"}
-                {:dt (LocalDateTime. 2010 1 1 0 5 0) :ns "boo"}
-                {:dt (LocalDateTime. 2010 1 1 0 26 0) :ns "boo"}
-                {:dt (LocalDateTime. 2010 1 1 0 27 0) :ns "boo"}
-                {:dt (LocalDateTime. 2010 1 1 0 27 0) :ns "boo2"}])))
+(deftest ordered-ns-trail
+  (let [now (LocalDateTime. 2010 1 1 0 30 0)
+        ns-trail [{:dt (LocalDateTime. 2010 1 1 0 14 0) :ns "boo3"}
+                  {:dt (LocalDateTime. 2010 1 1 0 5 0) :ns "boo"}
+                  {:dt (LocalDateTime. 2010 1 1 0 26 0) :ns "boo"}
+                  {:dt (LocalDateTime. 2010 1 1 0 27 0) :ns "boo"}
+                  {:dt (LocalDateTime. 2010 1 1 0 27 0) :ns "boo2"}]]
+    (testing "Ns score is assigned based on latest activity"
+      (is (= {"boo3" 0.7 "boo" 2.7 "boo2" 1.0}
+             (nses-with-score now ns-trail))))
+    (testing "Top nseses are those with the highest score"
+      (is (= ["boo" "boo2" "boo3"]
+             (top-nses now ns-trail))))))

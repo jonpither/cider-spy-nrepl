07235cf07857bb5cd80dd7781f636caba7b975e0
Jon Pither <jon.pither@gmail.com>
2014-07-14 18:28:33 +0100

    Tidying, moving connection code back into futures where it belongs.

--- src/cider_spy_nrepl/hub/client_facade.clj
+++ src/cider_spy_nrepl/hub/client_facade.clj
@@ -6,19 +6,24 @@
 (defn- send!
   [bootstrap op msg]
   (when bootstrap
-    (hubc/send! bootstrap (assoc msg :op op))))
+    (future
+      (hubc/send! bootstrap (assoc msg :op op)))))
 
 (defn register [session]
   (let [{:keys [hub-client hub-alias id]} @session]
     (send! hub-client :register {:alias hub-alias :session-id id})))
 
 (defn connect
-  "Connect to the hub and immediately register the current session."
-  [session host port]
-  (try
-    (hubc/connect host port session)
-    (catch java.net.SocketException e
-      (log/warn "Couldn't connect to HUB." host port))))
+  "Connect to the hub."
+  [session host port callback-fn]
+  (future
+    (when-let [hub-client
+               (try
+                 (hubc/connect host port session)
+                 (catch java.net.SocketException e
+                   (log/warn "Couldn't connect to HUB." host port)
+                   nil))]
+      (callback-fn hub-client))))
 
 (defn update-location
   "Update the location of where this developer is on the hub.
--- src/cider_spy_nrepl/middleware/cider_spy_hub.clj
+++ src/cider_spy_nrepl/middleware/cider_spy_hub.clj
@@ -11,68 +11,70 @@
 
 (defn- register
   "Register the alias for the users session on the CIDER-SPY-HUB."
-  [session msg]
+  [session]
   (cider/send-connected-msg! session (format "Setting alias on CIDER SPY HUB to %s." (:hub-alias @session)))
   (hub-client/register session))
+
+(defn- on-connect [session hub-client]
+  (if hub-client
+    (do
+      (sessions/update! session assoc :hub-client hub-client)
+      (cider/send-connected-msg! session "You are connected to the CIDER SPY HUB.")
+      (register session))
+    (cider/send-connected-msg! session "You are NOT connected to the CIDER SPY HUB.")))
 
 (defn- connect
   "Connect to the CIDER-SPY-HUB.
    Once connected, we attempt to register the user with an alias."
-  [session msg hub-host hub-port]
+  [session hub-host hub-port]
   (cider/send-connected-msg! session (format "Connecting to SPY HUB %s:%s." hub-host hub-port))
-  (if (:hub-client (sessions/update! session assoc :hub-client (hub-client/connect session hub-host hub-port)))
-    (do
-      (cider/send-connected-msg! session "You are connected to the CIDER SPY HUB.")
-      (register session msg))
-    (cider/send-connected-msg! session "You are NOT connected to the CIDER SPY HUB.")))
+  (hub-client/connect session hub-host hub-port (partial on-connect session)))
 
-(defn- connect-to-hub! [msg]
-  (when-let [session (sessions/session! msg)]
-    (let [{:keys [hub-client hub-alias]} @session
-          connected? (and hub-client (.isOpen (last hub-client)))
-          closed? (and hub-client (not connected?))]
-      (when (not connected?)
-        (if-let [[hub-host hub-port] (settings/hub-host-and-port)]
-          (do
-            (when closed?
-              (cider/send-connected-msg! session "SPY HUB connection closed, reconnecting"))
-            (connect session msg hub-host hub-port))
-          (cider/send-connected-msg! session "No CIDER-SPY-HUB host and port specified."))))))
+(defn- connect-to-hub! [session]
+  (let [{:keys [hub-client hub-alias]} @session
+        connected? (and hub-client (.isOpen (last hub-client)))
+        closed? (and hub-client (not connected?))]
+    (when (not connected?)
+      (if-let [[hub-host hub-port] (settings/hub-host-and-port)]
+        (do
+          (when closed?
+            (cider/send-connected-msg! session "SPY HUB connection closed, reconnecting"))
+          (connect session hub-host hub-port))
+        (cider/send-connected-msg! session "No CIDER-SPY-HUB host and port specified.")))))
 
 (defn- handle-register-hub-buffer-msg
   "We register the buffer in EMACS used for displaying connection information
    about the CIDER-SPY-HUB."
-  [{:keys [id hub-alias] :as msg}]
-  (when-let [session (sessions/session! msg)]
-    (sessions/update! session assoc :hub-connection-buffer-id (:id msg))
-    (when hub-alias
-      (sessions/update! session assoc :hub-alias hub-alias))))
+  [{:keys [id hub-alias] :as msg} session]
+  (sessions/update! session assoc :hub-connection-buffer-id (:id msg))
+  (when hub-alias
+    (sessions/update! session assoc :hub-alias hub-alias)))
 
 (defn- handle-change-hub-alias
   "Change alias in CIDER-SPY-HUB."
-  [{:keys [alias] :as msg}]
-  (when-let [session (sessions/session! msg)]
-    (sessions/update! session assoc :hub-alias alias)
-    (register session msg)))
+  [{:keys [alias] :as msg} session]
+  (sessions/update! session assoc :hub-alias alias)
+  (register session msg))
 
 (defn- handle-send-msg
   "Send a message to a developer registered on the CIDER-SPY-HUB."
-  [{:keys [recipient message] :as msg}]
-  (when-let [session (sessions/session! msg)]
-    (cider/send-connected-msg! session (format "Sending message to recipient %s on CIDER SPY HUB." recipient))
-    (hub-client/send-msg session recipient message)))
+  [{:keys [recipient message] :as msg} session]
+  (cider/send-connected-msg! session (format "Sending message to recipient %s on CIDER SPY HUB." recipient))
+  (hub-client/send-msg session recipient message))
 
 (defn wrap-cider-spy-hub
   [handler]
   (fn [{:keys [op] :as msg}]
-    (case op
-      "cider-spy-hub-connect" (handle-register-hub-buffer-msg msg)
-      "cider-spy-hub-alias" (handle-change-hub-alias msg)
-      "cider-spy-hub-send-msg" (handle-send-msg msg)
+    (if-let [session (sessions/session! msg)]
+      (case op
+        "cider-spy-hub-connect" (handle-register-hub-buffer-msg msg session)
+        "cider-spy-hub-alias" (handle-change-hub-alias msg session)
+        "cider-spy-hub-send-msg" (handle-send-msg msg session)
 
-      (do
-        (connect-to-hub! msg)
-        (handler msg)))))
+        (do
+          (connect-to-hub! session)
+          (handler msg)))
+      (handler msg))))
 
 (set-descriptor!
  #'wrap-cider-spy-hub

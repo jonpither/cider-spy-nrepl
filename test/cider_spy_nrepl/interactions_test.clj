9104593e73f9ca47796640eb56b522dc6e46c0b3
Jon Pither <jon.pither@gmail.com>
2014-07-18 08:51:34 +0100

    Converted interactions test to be e2e

--- test/cider_spy_nrepl/interactions_test.clj
+++ test/cider_spy_nrepl/interactions_test.clj
@@ -4,6 +4,9 @@
             [cider-spy-nrepl.hub.server-events :as server-events]
             [cider-spy-nrepl.hub.client-events :as client-events]
             [cider-spy-nrepl.middleware.cider :as cider]
+            [cider-spy-nrepl.hub.client :as client]
+            [cider-spy-nrepl.middleware.cider-spy-hub :as hub-middleware]
+            [cider-spy-nrepl.middleware.cider-spy :as spy-middleware]
             [clojure.tools.nrepl.transport :as transport]
             [cider-spy-nrepl.hub.register]
             [cheshire.core :as json]
@@ -11,89 +14,96 @@
   (:import [org.joda.time LocalDateTime]
            [io.netty.channel ChannelHandlerContext]))
 
-;; TODO - this bypasses all the handler code which is fair enough. Perhaps need a dedicated test?
-;; TODO - wider scope so that everything is client initiated rather than triggering server events
+;; todo reuse code across this and connections test
+;; todo get close connection working in the registrations example. This probable needs a close feature implemented.
+;; todo clj-refactor clean up the require
+;; todo add tests for sending msgs between sessions
+
+(defprotocol CanBeOpen
+  (isOpen [this])
+  (writeAndFlush [this m])
+  (sync [this]))
 
 (defmacro spy-harness [& body]
   `(do
+     (reset! cider-spy-nrepl.middleware.sessions/sessions {})
      (reset! cider-spy-nrepl.hub.register/sessions {})
      ~@body))
 
-(defn- end-to-end-fixture []
-  (let [nrepl-server-chan (chan)
-        cider-chan (chan)
-        hub-chan (chan)
-        hub-session (atom {:channel (reify ChannelHandlerContext
+(defn- stub-connect-to-hub [_ _ nrepl-session]
+  (let [hub-session (atom {:channel (reify ChannelHandlerContext
                                       (writeAndFlush [this msg]
-                                        (go (>! nrepl-server-chan msg))
-                                        nil))})
-        nrepl-session (atom {;; :summary-message-id "summary-msg-id" todo use this to ignore connection msgs?
-                             :session-started (LocalDateTime.)
-                             :transport (reify transport/Transport
-                                          (send [_ r]
-                                            (go (>! cider-chan r))
-                                            nil))})]
+                                        (client-events/process nrepl-session (clojure.edn/read-string msg))
+                                        nil))})]
+    [nil nil (reify CanBeOpen
+               (isOpen [this] true)
+               (writeAndFlush [this m]
+                 (server-events/process nil hub-session (clojure.edn/read-string m))
+                 nil)
+               (sync [this]
+                 this))]))
 
-    (go-loop []
-      (when-let [msg (<! nrepl-server-chan)]
-        (client-events/process nrepl-session (clojure.edn/read-string msg))
-        (recur))
-      (close! cider-chan))
+(defn- foo [session-id alias]
+  (let [cider-chan (chan)
+        cider-transport (reify transport/Transport
+                          (send [_ r]
+                            (when-not (= "connection-buffer-msg" (:id r))
+                              (go (>! cider-chan (:value r))))))]
 
-    (go-loop []
-      (when-let [m (<! hub-chan)]
-        (server-events/process nil hub-session m)
-        (recur))
-      (close! nrepl-server-chan))
+    ;; Handle a middleware request to connect to CIDER SPY HUB
+    ((hub-middleware/wrap-cider-spy-hub nil)
+     {:id "connection-buffer-msg"
+      :op "cider-spy-hub-connect"
+      :hub-alias alias
+      :session session-id
+      :transport cider-transport})
 
-    {:hub-session hub-session
-     :nrepl-session nrepl-session
-     :cider-chan cider-chan
-     :hub-chan hub-chan}))
+    (binding [client/connect stub-connect-to-hub]
+      ((hub-middleware/wrap-cider-spy-hub (constantly nil))
+       {:op "random-op"
+        :session session-id}))
+
+    cider-chan))
 
 (defn- cider-msg
   "Utility to extract string message sent to CIDER."
   [cider-chan]
-  (when-let [{:keys [value]} (first (alts!! [(timeout 2000) cider-chan]))]
+  (when-let [value (first (alts!! [(timeout 2000) cider-chan]))]
     (json/parse-string value true)))
 
-;; Test to ensure correctness of message sent to CIDER
-(deftest registration-should-bubble-to-cider
+(deftest alias-should-bubble-to-cider
   (spy-harness
-   (let [{:keys [hub-chan cider-chan]} (end-to-end-fixture) ]
-     (>!! hub-chan {:op :register :alias "Jon" :session-id 1})
+   (let [cider-chan (foo 1 "Jon")]
+     (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg cider-chan))))
 
-     (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg cider-chan)))))))
+     ((hub-middleware/wrap-cider-spy-hub nil)
+      {:op "cider-spy-hub-alias"
+       :alias "Jon2"
+       :session 1})
+
+     (is (= {:1 {:alias "Jon2" :nses []}} (:devs (cider-msg cider-chan)))))))
 
 (deftest user-registrations
   (spy-harness
-   (let [fixture1 (end-to-end-fixture)
-         fixture2 (end-to-end-fixture)]
+   (let [cider-chan1 (foo 1 "Jon")]
+     (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg cider-chan1))))
+     (let [cider-chan2 (foo 2 "Dave")]
+       (is (= {:1 {:alias "Jon" :nses []} :2 {:alias "Dave" :nses []}} (:devs (cider-msg cider-chan1))))
+       (is (= {:1 {:alias "Jon" :nses []} :2 {:alias "Dave" :nses []}} (:devs (cider-msg cider-chan2)))))
 
-     (>!! (:hub-chan fixture1) {:session-id 1 :op :register :alias "Jon"})
-     (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg (:cider-chan fixture1)))))
-
-     (>!! (:hub-chan fixture2) {:session-id 2 :op :register :alias "Dave"})
-     (is (= {:1 {:alias "Jon" :nses []} :2 {:alias "Dave" :nses []}} (:devs (cider-msg (:cider-chan fixture1)))))
-     (is (= {:1 {:alias "Jon" :nses []} :2 {:alias "Dave" :nses []}} (:devs (cider-msg (:cider-chan fixture2)))))
-
-     (>!! (:hub-chan fixture2) {:op :unregister})
-     (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg (:cider-chan fixture1))))))))
+     ;; (>!! (:hub-chan fixture2) {:op :unregister})
+     ;; (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg (:cider-chan fixture1)))))
+)))
 
 (deftest dev-locations
   (spy-harness
-   (let [{:keys [hub-chan cider-chan]} (end-to-end-fixture)]
-
-     (>!! hub-chan {:session-id 1 :op :register :alias "Jon"})
+   (let [cider-chan (foo 1 "Jon")]
      (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg cider-chan))))
 
-     (>!! hub-chan {:op :location :alias "Jon" :ns "foo" :dt (java.util.Date.)})
-     (is (= {:1 {:alias "Jon" :nses ["foo"]}} (:devs (cider-msg cider-chan)))))))
+     ((spy-middleware/wrap-cider-spy (constantly nil))
+      {:op "load-file"
+       :file "(ns foo.bar) (println \"hi\")"
+       :session 1})
 
-(deftest change-alias
-  (spy-harness
-   (let [{:keys [hub-chan cider-chan]} (end-to-end-fixture)]
-     (>!! hub-chan {:session-id 1 :op :register :alias "Jon"})
      (is (= {:1 {:alias "Jon" :nses []}} (:devs (cider-msg cider-chan))))
-     (>!! hub-chan {:session-id 1 :op :register :alias "Jon2"})
-     (is (= {:1 {:alias "Jon2" :nses []}} (:devs (cider-msg cider-chan)))))))
+     (is (= {:1 {:alias "Jon" :nses ["foo.bar"]}} (:devs (cider-msg cider-chan)))))))

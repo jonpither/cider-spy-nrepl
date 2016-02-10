dc8c17ab5eab62776396fddcda069cc770cb9e57
Jon Pither <jon.pither@gmail.com>
2016-02-10 18:20:54 +0000

    Remove & fixed the issue

--- core/src/cider_spy_nrepl/middleware/cider_spy_multi_repl.clj
+++ core/src/cider_spy_nrepl/middleware/cider_spy_multi_repl.clj
@@ -6,23 +6,20 @@
 (deftype TrackingTransport [transport]
   nrepl-transport/Transport
   (send [this msg]
-    (println "Outgoing" msg)
     (nrepl-transport/send transport msg))
-  (recv [this]
-    (println "Incoming"))
-  (recv [this timeout]
-    (println "Incoming" timeout)))
+  (recv [this])
+  (recv [this timeout]))
 
 (defn wrap-multi-repl
   "Multi REPL Middleware - CURRENTLY NEVER GETS CALLED"
   [handler]
   (println "SETTING")
   (fn [{:keys [transport] :as msg}]
-    (println "MULTI-REPL")
+    (println "MULTI-REPL" (:code msg))
     (handler (assoc msg :transport (TrackingTransport. transport)))))
 
 (set-descriptor!
  #'wrap-multi-repl
- {:requires #{#'interruptible-eval}
+ {:expects #{#'interruptible-eval}
   :handles {"cider-spy-multi-repl" {:doc "See the cider-spy README"
                                     :returns {} :requires {}}}})

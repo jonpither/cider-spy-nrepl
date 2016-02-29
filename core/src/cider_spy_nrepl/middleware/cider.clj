cea6f153c72d3b274f0f38a13b311007e269bb04
Jon Pither <jon.pither@gmail.com>
2016-02-29 18:40:23 +0000

    More defensive guards, MR fixes

--- core/src/cider_spy_nrepl/middleware/cider.clj
+++ core/src/cider_spy_nrepl/middleware/cider.clj
@@ -1,7 +1,7 @@
 (ns cider-spy-nrepl.middleware.cider
   (:require [cheshire.core :as json]
             [cider-spy-nrepl.middleware.summary-builder :as summary-builder]
-            [cider-spy-nrepl.middleware.session-vars :refer [*summary-message-id* *hub-connection-buffer-id* *cider-spy-transport* *watch-session-request-id*]]
+            [cider-spy-nrepl.middleware.session-vars :refer :all]
             [clojure.tools.nrepl.misc :refer [response-for]]
             [clojure.tools.nrepl.transport :as transport]))
 
@@ -24,11 +24,13 @@
 
 (defn send-connected-on-hub-msg!
   [session alias]
-  (send! session {:id (@session #'*hub-connection-buffer-id*) :hub-registered-alias alias}))
+  (when-let [connection-buffer-message-id (@session #'*hub-connection-buffer-id*)]
+    (send! session {:id connection-buffer-message-id :hub-registered-alias alias})))
 
 (defn send-connected-msg!
   "Send a message back to CIDER-SPY pertaining to CIDER-SPY-HUB connectivity.
    The correct ID is used as to ensure the message shows up in the relevant
    CIDER-SPY buffer."
   [session s]
-  (send! session {:id (@session #'*hub-connection-buffer-id*) :value (str "CIDER-SPY-NREPL: " s) :printed-value "true"}))
+  (when-let [connection-buffer-message-id (@session #'*hub-connection-buffer-id*)]
+    (send! session {:id connection-buffer-message-id :value (str "CIDER-SPY-NREPL: " s) :printed-value "true"})))

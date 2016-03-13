3742275d701058829e2004ccba4d7a9507118bed
Jon Pither <jon.pither@gmail.com>
2016-03-13 09:17:13 +0000

    Fixes main startup

--- core/src/cider_spy_nrepl/hub/main.clj
+++ core/src/cider_spy_nrepl/hub/main.clj
@@ -2,10 +2,14 @@
   (:gen-class))
 
 (defn -main [& args]
-  (let [port (Integer/parseInt (or (first args) "7771"))]
-    ;; We eval so that we don't AOT anything beyond this class
-    (eval '(do (require 'cider-spy-nrepl.hub.server)
-
-               (let [[b] (start port)]
-                 (.sync b))
-               (println "Exiting CIDER-SPY HUB")))))
+  (def port (Integer/parseInt (or (first args) "7771")))
+  ;; We eval so that we don't AOT anything beyond this class, for REPL reloaded reasons
+  (eval '(do (require 'cider-spy-nrepl.hub.server)
+             (-> cider-spy-nrepl.hub.main/port
+                 cider-spy-nrepl.hub.server/start
+                 first
+                 (.sync)
+                 (.channel)
+                 (.closeFuture)
+                 (.sync))
+             (println "Exiting CIDER-SPY HUB"))))

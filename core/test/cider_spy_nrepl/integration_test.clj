52a081ad616c8398bcd7e2d736f49516446d3d11
Jon Pither <jon.pither@gmail.com>
2016-03-23 08:03:32 +0000

    Test simplifiy

--- core/test/cider_spy_nrepl/integration_test.clj
+++ core/test/cider_spy_nrepl/integration_test.clj
@@ -1,34 +1,13 @@
 (ns cider-spy-nrepl.integration-test
-  (:require [clojure.tools.nrepl.server :as nrserver]
-            [cider-spy-nrepl.middleware.cider-spy]
-            [cider-spy-nrepl.hub.server :as hub-server]
-            [clojure.tools.nrepl.transport :as transport]
-            [cider-spy-nrepl.middleware.cider-spy-multi-repl]
-            [cider-spy-nrepl.middleware.cider-spy-hub]
-            [cider-spy-nrepl.middleware.hub-settings :as hub-settings]
+  (:require [cheshire.core :as json]
+            [cider-spy-nrepl
+             [nrepl-test-utils :refer [messages-chan! take-from-chan!]]
+             [test-utils :refer [wrap-startup-nrepl-server]]]
             [clojure.test :refer :all]
             [clojure.tools.nrepl :as nrepl]
-            [cheshire.core :as json]))
+            [clojure.tools.nrepl.transport :as transport]))
 
-(defn- start-up-repl-server []
-  (let [server
-        (nrserver/start-server
-         :port 7777
-         :handler (nrserver/default-handler
-                   ;; #'cider-spy-nrepl.middleware.cider-spy-multi-repl/wrap-multi-repl
-                   ;; #'cider-spy-nrepl.middleware.cider-spy-hub/wrap-cider-spy-hub
-                   #'cider-spy-nrepl.middleware.cider-spy/wrap-cider-spy))]
-    server))
-
-(defn- stop-repl-server [server]
-  (nrserver/stop-server server))
-
-(defn- wrap-setup-once [f]
-  (let [server (start-up-repl-server)]
-    (f)
-    (stop-repl-server server)))
-
-(use-fixtures :once wrap-setup-once)
+(use-fixtures :each wrap-startup-nrepl-server)
 
 (defn- nrepl-message
   ([timeout tr payload]
@@ -41,37 +20,39 @@
         response (nrepl-message transport {:ns "user" :op "eval" :code "( + 1 1)" :file "*cider-repl blog*" :line 12 :column 6 :id 14})]
     (is (= ["done"] (:status (second response))))))
 
-(defn- send-and-seq [transport msg]
-  (-> (transport/send transport msg)
-      (nrepl/response-seq 5000)))
-
 (deftest test-display-a-summary
   (let [transport (nrepl/connect :port 7777 :host "localhost")
-        session-id (:new-session (first (nrepl/response-seq (transport/send transport {:op "clone" :id "session-create-id"}) 5000)))]
-    (assert session-id)
+        msgs-chan (messages-chan! transport)]
 
-    (is (= [:devs nil] (-> (transport/send transport {:session session-id :id "session-msg-ig" :op "cider-spy-summary"})
-                           (nrepl/response-seq 5000)
-                           first
-                           :value
-                           (json/parse-string keyword)
-                           (find :devs))))
+    (transport/send transport {:op "clone" :id "session-create-id"})
 
-    (let [responses (->> {:session session-id :ns "clojure.string" :op "eval" :code "( + 1 1)" :file "*cider-repl blog*" :line 12 :column 6 :id 14}
-                         (send-and-seq transport))]
-      (is (= [{:id 14,
-               :ns "clojure.string",
-               :session session-id
-               :value "2"}
-              {:id 14,
-               :session session-id,
-               :status ["done"]}]
-             (take 2 (filter #(= 14 (:id %)) responses))))
+    (let [session-id (->> msgs-chan (take-from-chan! 1 1000) first :new-session)]
+      (assert session-id)
 
-      (is (= "clojure.string" (-> responses
-                                  (nth 2)
-                                  :value
-                                  (json/parse-string keyword)
-                                  :ns-trail
-                                  first
-                                  :ns))))))
+      (transport/send transport {:session session-id :id "session-msg-ig" :op "cider-spy-summary"})
+
+      (is (= [:devs nil] (-> (->> msgs-chan (take-from-chan! 1 1000))
+                             first
+                             :value
+                             (json/parse-string keyword)
+                             (find :devs))))
+
+      (transport/send transport {:session session-id :ns "clojure.string" :op "eval" :code "( + 1 1)" :file "*cider-repl blog*" :line 12 :column 6 :id 14})
+
+      (let [responses (->> msgs-chan (take-from-chan! 3 2000))]
+        (is (= [{:id 14,
+                 :ns "clojure.string",
+                 :session session-id
+                 :value "2"}
+                {:id 14,
+                 :session session-id,
+                 :status ["done"]}]
+               (take 2 (filter #(= 14 (:id %)) responses))))
+
+        (is (= "clojure.string" (-> responses
+                                    (nth 2)
+                                    :value
+                                    (json/parse-string keyword)
+                                    :ns-trail
+                                    first
+                                    :ns)))))))
